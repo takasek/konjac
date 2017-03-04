9b80c2fb6e97e28738a5813888e8f52b35544fd1
高松幸平 <gaoxin.xixxix@gmail.com>
2017-03-04 13:53:16 +0900

    add a class for the phrases tableviw

---
+++ Konjac/Konjac/PhrasesViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PhrasesViewController.swift
+//  Konjac
+//
+//  Created by 高松幸平 on 2017/03/04.
+//  Copyright © 2017年 trySwiftHackathon. All rights reserved.
+//
+
+import UIKit
+
+class PhrasesViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension PhrasesViewController: UITableViewDelegate {
+    
+}
+
+extension PhrasesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell(frame: CGRect.zero)
+    }
+}
