00e10c765005408ddfee828911a0cf6fd99c5960
Yoshitaka Seki <takassekiyoshi@gmail.com>
2017-03-04 14:45:08 +0900

    Add GoogleTranslate.storyboard and ViewController

---
+++ Konjac/Konjac/GoogleTranslateViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GoogleTranslateViewController.swift
+//  Konjac
+//
+//  Created by Yoshitaka Seki on 2017/03/04.
+//  Copyright © 2017年 trySwiftHackathon. All rights reserved.
+//
+
+import Foundation
